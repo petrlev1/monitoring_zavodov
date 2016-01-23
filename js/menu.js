a6e4b3c20d9075f929a094bce6c72fb7df00cd97
Petrlev <levendeevp@gmail.com>
2016-01-23 17:09:48 +0300

    test menu

---
+++ js/menu.js
@@ -0,0 +1,77 @@
+(function(){
+  
+  //angular module
+  var myApp = angular.module('myApp', ['angularTreeview']);
+
+  //test controller
+  myApp.controller('myController', function($scope){
+
+  	//test tree model 1
+    $scope.roleList1 = [
+        { "roleName" : "ОАО РосСпиртПром", "roleId" : "role1", "children" : [
+          { "roleName" : "subUser1", "roleId" : "role11", "children" : [] },
+          { "roleName" : "subUser2", "roleId" : "role12", "children" : [
+            { "roleName" : "subUser2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subUser2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subUser2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Admin", "roleId" : "role2", "children" : [] },
+
+        { "roleName" : "Guest", "roleId" : "role3", "children" : [] }
+      ];
+      
+      
+      
+
+  	//test tree model 2
+    $scope.roleList2 = [
+        { "roleName" : "User", "roleId" : "role1", "children" : [
+          { "roleName" : "subUser1", "roleId" : "role11", "collapsed" : true, "children" : [] },
+          { "roleName" : "subUser2", "roleId" : "role12", "collapsed" : true, "children" : [
+            { "roleName" : "subUser2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subUser2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subUser2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Admin", "roleId" : "role2", "children" : [
+          { "roleName" : "subAdmin1", "roleId" : "role11", "collapsed" : true, "children" : [] },
+          { "roleName" : "subAdmin2", "roleId" : "role12", "children" : [
+            { "roleName" : "subAdmin2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subAdmin2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subAdmin2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Guest", "roleId" : "role3", "children" : [
+          { "roleName" : "subGuest1", "roleId" : "role11", "children" : [] },
+          { "roleName" : "subGuest2", "roleId" : "role12", "collapsed" : true, "children" : [
+            { "roleName" : "subGuest2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subGuest2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subGuest2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]}
+      ];
+
+      
+      
+    //roleList1 to treeview
+    $scope.roleList = $scope.roleList1;
+  
+  });
+  
+})();
+
+
+/*
+    angular.treeview.js
+*/
+(function(l){l.module("angularTreeview",[]).directive("treeModel",function($compile){return{restrict:"A",link:function(a,g,c){var e=c.treeModel,h=c.nodeLabel||"label",d=c.nodeChildren||"children",k='<ul><li data-ng-repeat="node in '+e+'"><i class="collapsed" data-ng-show="node.'+d+'.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.'+d+'.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.'+
+d+'.length"></i> <span data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.'+h+'}}</span><div data-ng-hide="node.collapsed" data-tree-model="node.'+d+'" data-node-id='+(c.nodeId||"id")+" data-node-label="+h+" data-node-children="+d+"></div></li></ul>";e&&e.length&&(c.angularTreeview?(a.$watch(e,function(m,b){g.empty().html($compile(k)(a))},!1),a.selectNodeHead=a.selectNodeHead||function(a,b){b.stopPropagation&&b.stopPropagation();b.preventDefault&&b.preventDefault();b.cancelBubble=
+!0;b.returnValue=!1;a.collapsed=!a.collapsed},a.selectNodeLabel=a.selectNodeLabel||function(c,b){b.stopPropagation&&b.stopPropagation();b.preventDefault&&b.preventDefault();b.cancelBubble=!0;b.returnValue=!1;a.currentNode&&a.currentNode.selected&&(a.currentNode.selected=void 0);c.selected="selected";a.currentNode=c}):g.html($compile(k)(a)))}}})})(angular);
